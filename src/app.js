a404ee9441d969db807b6bd740fd5ffa82fb5d48
zdev <ztudiodev@gmail.com>
2026-05-17 20:50:44 -0500

    Preparando deploy para Render

--- src/app.js
+++ src/app.js
@@ -13,6 +13,10 @@ const analysisRoutes = require("./routes/analysisRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
