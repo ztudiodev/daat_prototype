5a45a612cbaedabf57db1a7e407e41c1b30f5f6b
zdev <ztudiodev@gmail.com>
2026-05-17 21:08:10 -0500

    Preparando deploy para supabase

--- src/config/database.js
+++ src/config/database.js
@@ -6,11 +6,17 @@ if (!databaseUrl) {
 	throw new Error("DATABASE_URL is required in .env");
 }
 
-const sequelize = new Sequelize(databaseUrl, {
-	dialect: "postgres",
-	logging: false,
-	dialectOptions:
-		process.env.DB_SSL === "true" ? { ssl: { rejectUnauthorized: false } } : {},
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
+
+
 
 module.exports = sequelize;
