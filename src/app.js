4029ae39459bb391778baf5af21626568f818640
zdev <ztudiodev@gmail.com>
2026-05-17 21:58:38 -0500

    nueva correccion en app.js

--- src/app.js
+++ src/app.js
@@ -13,9 +13,6 @@ const analysisRoutes = require("./routes/analysisRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
